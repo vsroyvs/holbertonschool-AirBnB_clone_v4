4d89358dfe89387133163053776b982666a6a470
Roy <vargas.sap321@gmail.com>
2023-11-10 12:46:45 -0500

    feat: change css properties

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,27 +1,25 @@
 $(() => {
-    $('input').css({'margin-right': '10px'});
-    let amenitiesChecked = {};
-  
-    $("input:checkbox").on('change', function () {
-      if ($( this ).prop('checked')){
-        amenitiesChecked[$( this ).data('id')] = $( this ).data('name');
-      } else {
-        delete amenitiesChecked[$(this).data('id')];
-      }
-      $('.amenities h4').text(Object.values(amenitiesChecked).join(', '));
-    });
+  $('input').css({'margin-right': '10px'});
+  let amenitiesChecked = {};
 
-    $.ajax({
-      url: 'http://0.0.0.0:5001/api/v1/status/',
-      type: 'GET',
-      dataType: 'json',
-      success: (data) => {
-        if (data.status === 'OK') {
-          $('#api_status').addClass('available');
-        } else {
-          $('#api_status').removeClass('available');
-        }
-      }
-    })
+  $("input:checkbox").on('change', function () {
+    if ($( this ).prop('checked')){
+      amenitiesChecked[$( this ).data('id')] = $( this ).data('name');
+    } else {
+      delete amenitiesChecked[$(this).data('id')];
+    }
+    $('.amenities h4').text(Object.values(amenitiesChecked).join(', '));
   });
-  
+
+  $.ajax({
+    url: 'http://127.0.0.1:5001/api/v1/status/',
+    type: 'GET',
+    dataType: 'json'
+  }).done((data) => {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
